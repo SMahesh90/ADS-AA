d328d5fb0d208ff79037cac36126c447b5872f29
SMahesh90 <166918497+SMahesh90@users.noreply.github.com>
2025-04-15 19:34:46 +0530

    knapSackDP.java

--- knapSackDP.java
+++ knapSackDP.java
@@ -1,28 +1,27 @@
+import java.util.*;
+
 public class KnapSack {
-    public int solve(int[] weights, int[] values, int capacity) {
-        int n = weights.length;
-        int[][] dp = new int[n + 1][capacity + 1];
-        for (int i = 1; i <= n; i++) {
-            for (int w = 0; w <= capacity; w++) {
-                if (weights[i - 1] <= w) {
-                    // Include the item
-                    dp[i][w] = Math.max(dp[i - 1][w], dp[i - 1][w - weights[i - 1]] + values[i - 1]);
-                } else {
-                    // Exclude the item
-                    dp[i][w] = dp[i - 1][w];
+    public int solve(int []w,int []v,int cap){
+        int n = w.length;
+        int [][]dp = new int[n+1][cap + 1];
+        for (int i=1;i<=n;i++){
+            for (int j=0;j<=cap;j++){
+                if (w[i-1] <= j){
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i-1][j-w[i-1]]+v[i-1]);
+                }else{
+                    dp[i][j] = dp[i-1][j];
                 }
             }
         }
-
-        return dp[n][capacity];
+        return dp[n][cap];
     }
-
+    
     public static void main(String[] args) {
-        int[] weights = {1, 2, 3}; 
-        int[] values = {10, 15, 40}; 
-        int capacity = 6; 
+        int[] w = {1, 2, 3}; 
+        int[] v = {10, 15, 40}; 
+        int cap = 6; 
         KnapSack knapSack = new KnapSack();
-        int maxValue = knapSack.solve(weights, values, capacity);
+        int maxValue = knapSack.solve(w, v, cap);
         System.out.println("Max value: " + maxValue);
     }
 }
